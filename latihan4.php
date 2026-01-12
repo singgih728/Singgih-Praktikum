ab0fd4be901a6c0ba5c2b4144a7cd7dfff65372f
singgih728 <rsinggih728@gmail.com>
2026-01-12 12:42:03 +0700

    Add latihan4.php with basic PHP variables

---
+++ latihan4.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>LATIHAN 4 PEMROGRAMAN WEB</title>
+    </head>
+    <body>
+        <?php
+            $bilanganbulat = 17;
+            $teks = "aku";
+            $bilangandesimal = 17.42;
+
+            echo "Nilai Variabel bilangan bulat : ". $bilanganbulat. "<br/>";
+            echo "Nilai Variabel teks : ". $teks. "<br/>";
+            echo "Nilai Variabel bilangan desimal : ". $bilangandesimal. "<br/>";
+        ?>
+    </body>
+</html>
