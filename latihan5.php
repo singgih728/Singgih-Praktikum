2c87f0859edd3748ade8fd980d68ad2cb1255298
singgih728 <rsinggih728@gmail.com>
2026-01-12 12:43:39 +0700

    Add latihan5.php for web programming exercise

---
+++ latihan5.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>LATIHAN 5 PEMROGRAMAN WEB</title>
+    </head>
+    <body>
+        <?php
+            $nim = "2305101075";
+            $nama = "Singgih Rohniadi";
+            $angka1 = 10;
+            $angka2 = 15;
+
+            $hasilpenjumlahan = $angka1 + $angka2;
+
+            echo "NIM : ". $nim. "<br/>";
+            echo "Nama : ". $nama. "<br/>";
+            echo "Hasil Penjumlahan dari " . $angka1. " dan " . $angka2. " = " . $hasilpenjumlahan."<br/>";
+        ?>
+    </body>
+</html>
