aff36eacaaffe9ddc9f579ac6ded428ad1df66d4
singgih728 <rsinggih728@gmail.com>
2026-01-12 12:53:30 +0700

    Add PHP script for multidimensional arrays example

---
+++ latihan9.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>LATIHAN 8 PEMROGRAMAN WEB</title>
+    </head>
+    <body>
+        <?php
+            #cara pertama mendeklarasikan array multidimensi
+            print ("CARA PERTAMA MENDEKLARASIKAN ARRAY MULTIDIMENDI: <br/>");
+            $buah = array(
+                    "apel"=> array("warna"=>"merah", "rasa"=>"manis"),
+                    "pisang"=> array("warna"=>"kuning", "rasa"=>"manis")
+            );
+            print "Warna buah apel adalah ";
+            print $buah["apel"]["warna"]."<br/>";
+            print "Warna buah pisang adalah ";
+            print $buah["pisang"]["warna"]."<br/>";
+
+            echo "<br/><br/><br/>";
+
+            #cara kedua mendeklarasikan array multidimensi
+            print ("CARA KEDUA MENDEKLARASIKAN ARRAY MULTIDIMENSI: <br/>");
+            $buah = array(
+                    array("apel", "merah", "manis"),
+                    array("pisang", "kuning", "manis")
+            );
+            
+            echo $buah[0][0]." warna: ".$buah[0][1]." rasa: ".$buah[0][2]."<br/>";
+            echo $buah[1][0]." warna: ".$buah[1][1]." rasa: ".$buah[1][2]."<br/>";
+        ?>
+    </body>
+</html>
